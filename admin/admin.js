d5814fb1590701f594664b2a2b7cb6e2465653a4
anish-developer <anishdeveloper007@gmail.com>
2022-06-30 11:46:59 +0530

    update function in admin

--- admin/admin.js
+++ admin/admin.js
@@ -15,6 +15,7 @@ router.post('/login',upload.none(), async (req,res)=>{
     // res.send('hii')
     const email = req.body.email
     const password = req.body.password
+    console.log(email,password)
     try {
         const data = await Admin.findOne({a_email: email})
         // console.log(data)
@@ -115,6 +116,64 @@ router.patch('/reject/jobseeker/:_id?', async (req,res)=>{
         res.status(201).json('reject')
     } catch (error) {
         res.status(400).json('invalid id')
+    }
+})
+
+// edit employers
+router.put('/edit/employer/:_id?',upload.none(),async (req,res)=>{
+    try {
+        const id = req.params._id
+        const name= req.body.c_name
+        const address= req.body.c_address
+        const person= req.body.c_person
+        const number= req.body.c_number
+        const email= req.body.c_email
+        const description= req.body.c_description
+        // console.log(email,name,address,person,number,description)
+        const data = await Employer.updateOne(
+            {_id:id},
+            {
+                $set:{
+                    c_name:name,
+                    c_address:address,
+                    c_person:person,
+                    c_number:number,
+                    c_email:email,
+                    c_description:description
+                }
+            }
+        )
+        res.status(201).json(data.acknowledged)
+    } catch (error) {
+        res.status(400).json('invalid id')
+    }
+})
+
+
+// edit JobSeeker
+router.put('/edit/jobseeker/:_id?',upload.none(),async (req,res)=>{
+    try {
+        const id = req.params._id
+        console.log(id)
+        const name= req.body.j_name
+        const number= req.body.j_number
+        const email= req.body.j_email
+        const address= req.body.j_address
+        // console.log(name,number,email,address)
+        const data = await JobSeeker.updateOne(
+            {_id:id},
+            {
+                $set:{
+                    j_name:name,
+                    j_number:number,
+                    j_email:email,
+                    j_address:address
+                }
+            }
+        )
+            res.status(201).json(data.acknowledged)
+    } catch (error) {
+        res.status(400).json("invalid id")
     }
 })
 
